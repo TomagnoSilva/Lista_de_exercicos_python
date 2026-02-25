3c1ec071e7c75245e74a9921fa77f24cce719d79
TomagnoSilva <tomagnosantos1@gmail.com>
2026-02-25 08:53:04 -0300

    exercicio_03 alteração de codigo

--- Estrutura_sequencial/exercicio_03.py
+++ Estrutura_sequencial/exercicio_03.py
@@ -8,5 +8,5 @@ numero_2 = int(input("Digite um número: "))
 print(numero_1 + numero_2)
 
 # outra forma:
-resultado = numero_1 + numero_2
-print(f"A soma dos número é: {resultado}")
+soma = numero_1 + numero_2
+print(f"A soma entre {numero_1} e {numero_2} é: {soma}")
