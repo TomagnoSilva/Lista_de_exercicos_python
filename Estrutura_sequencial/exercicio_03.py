441cd4f0f1617116af134536663f5d72d5afe0cd
TomagnoSilva <tomagnosantos1@gmail.com>
2026-02-25 08:45:26 -0300

    exercicio_03 realizado

---
+++ Estrutura_sequencial/exercicio_03.py
@@ -0,0 +1,12 @@
+"""
+Faça um programa que peça dois números e imprima a soma:
+"""
+
+numero_1 = int(input("Digite um número: "))
+numero_2 = int(input("Digite um número: "))
+
+print(numero_1 + numero_2)
+
+# outra forma:
+resultado = numero_1 + numero_2
+print(f"A soma dos número é: {resultado}")
