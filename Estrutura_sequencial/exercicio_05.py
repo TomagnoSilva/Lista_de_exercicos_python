ee022b68f516e06b4a9c84b20d56bd6ca90f41d4
TomagnoSilva <tomagnosantos1@gmail.com>
2026-02-27 08:23:22 -0300

    exer_05 realizado

---
+++ Estrutura_sequencial/exercicio_05.py
@@ -0,0 +1,12 @@
+"""
+Faça um programa que converta metros para centímetros:
+"""
+
+# Solicita o valor em metros ao usuário
+metros = float(input("Entre com os métros: "))
+
+# Realiza a conversão (1 metro = 100 centímetros)
+centimetros = metros * 100
+
+# Exibe o resultado de forma clara ao usuário
+print(f"{metros} metros equivalem a {centimetros:.0f} em centímetros")
